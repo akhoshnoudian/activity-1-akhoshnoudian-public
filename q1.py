662c05e06d5869deb65e7e25e408f5c3f4fc11c6
akhoshnoudian <122493064+akhoshnoudian@users.noreply.github.com>
2025-01-27 18:08:25 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,13 @@
+def isPrime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def getPrimeNumbers(n):
+    return [num for num in range(2, n + 1) if isPrime(num)]
+
+print(getPrimeNumbers(20))
