c7fc218d9cc710ae5695493ba2dedddb4ee59170
akhoshnoudian <122493064+akhoshnoudian@users.noreply.github.com>
2025-01-27 18:09:39 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,25 @@
 def wordCount(t):
+    word_dict = {}  
+    
+    try:
+        with open(t, 'r') as file:
+            
+            for line_num, line in enumerate(file, 1):
+                words = line.split()  # Split line into words
+                
+                for word in words:
+                    # If the word is not in the dictionary, add it with the line number
+                    if word not in word_dict:
+                        word_dict[word] = [line_num]
+                    else:
+                        if line_num not in word_dict[word]:
+                            word_dict[word].append(line_num)
+                 
+        
+        return word_dict  # Return the dictionary with word occurrences
+    
+    except FileNotFoundError:
+        print(f"The file '{t}' was not found.")
+        return {}
+
+print(wordCount("words.txt"))
